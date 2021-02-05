7402945da50f923a87059992aee0653527371ad2
kelvin <hanqi_kelvin@163.com>
2021-02-05 11:31:44 +0800

    2021-2-5 11:30

--- spark-core/src/main/scala/com/yykj/spark/core/mock/SparkMockDriver.scala
+++ spark-core/src/main/scala/com/yykj/spark/core/mock/SparkMockDriver.scala
@@ -15,16 +15,17 @@ object SparkMockDriver {
 
     // TODO: 1.待计算数据及业务规则
     var datas : List[Int] = List(1,2,3,4,5)
-    var logic : (Int) => Int = {_ * 10}
+    // Spark RDD 计算逻辑不可改变，不可外部传递计算逻辑
+    //var logic : (Int) => Int = {_ * 10}
 
     // TODO: 2.创建作业信息,自定义数据及处理逻辑
     val rdd = new SparkMockRDD()
     rdd.datas = datas
-    rdd.logic = logic
+    //rdd.logic = logic
 
-    // TODO: 3.切片生成executor task
-    var taskes : mutable.Queue[SparkMockTask] = createTaskes(rdd)
-    if(taskes.length <= 0){
+    // TODO: 3.切片生成分区(executor task)
+    var partition_list : mutable.Queue[SparkMockTask] = rdd.splitPartition()
+    if(partition_list.length <= 0){
       println("Driver(客户端)生成Executor Tasks失败...")
     }
     else{
@@ -34,7 +35,7 @@ object SparkMockDriver {
     // TODO: 4.发送数据
     var client : Socket = null
 
-    for(num <- 0 to taskes.length - 1){
+    for(num <- 0 to partition_list.length - 1){
       // TODO: 连接服务器
       if(num == 0){
         client = new Socket("localhost", 9999)
@@ -47,7 +48,7 @@ object SparkMockDriver {
       // TODO: 发送数据
       val os = client.getOutputStream
       val obj_os = new ObjectOutputStream(os)
-      obj_os.writeObject(taskes(num))
+      obj_os.writeObject(partition_list(num))
       obj_os.flush()
       os.close()
       obj_os.close()
@@ -57,54 +58,5 @@ object SparkMockDriver {
 
     // TODO: 4.通知
     print("Driver(客户端)数据发送完毕...")
-  }
-
-  /**
-   * 生成task任务集合
-   */
-  def createTaskes(rdd: SparkMockRDD): mutable.Queue[SparkMockTask] = {
-
-    var datas = rdd.datas
-    var logic = rdd.logic
-    var num_executor = 2
-
-    // TODO: 判断条件
-    if (datas == null || logic == null || num_executor <= 0) {
-      null
-    }
-
-    // TODO: 生成subtask列表
-    var split_num = datas.length / num_executor
-    var task_list: mutable.Queue[SparkMockTask] = new mutable.Queue[SparkMockTask]()
-
-    var data_start = 0
-    var data_end = 0
-    for (num <- 0 to num_executor - 1) {
-      // TODO: 模拟分区
-      data_start = data_start + (split_num * num)
-      if(data_start < 0){
-        data_start = 0
-      }
-      data_end = split_num * (num + 1) - 1
-      if (data_end + split_num > datas.length - 1){
-        data_end = datas.length - 1
-      }
-
-      var data_len : Int = data_end - data_start + 1
-      var task_datas: Array[Int] = new Array[Int](data_len)
-      var arr_index : Int = 0
-      for(index <- data_start to data_end){
-        task_datas(arr_index) = datas(index)
-        arr_index = arr_index + 1
-      }
-
-      // TODO:生成Executor
-      var task = new SparkMockTask
-      task.logic = logic
-      task.datas = task_datas.toList
-      task_list += task
-    }
-
-    task_list
   }
 }
--- spark-core/src/main/scala/com/yykj/spark/core/mock/SparkMockRDD.scala
+++ spark-core/src/main/scala/com/yykj/spark/core/mock/SparkMockRDD.scala
@@ -3,22 +3,79 @@ package com.yykj.spark.core.mock
 import scala.collection.mutable
 
 /**
- * Spark RDD:数据结构 最小计算单元（进行组合） 用于封装全量数据及应用逻辑
- *
+ * Spark RDD:
+ * 1.具备弹性：存储、容错、计算、分区
+ * 2.分布式：数据存储在分布式大数据平台 RDD进行分布式计算；
+ * 3.计算：RDD是Spark最小计算单元，可以通过RDD组合完成丰富功能
+ * 4.数据模型：用于封装计算逻辑，不保存数据
+ * 5.抽象类
+ * 6.不可变：RDD封装了计算逻辑，不可以改变。如需新计算逻辑需要编写新的RDD
+ * 7.可分区：实现并行计算
  */
 class SparkMockRDD extends Serializable {
 
   /**
+   * 分区数量
+   */
+  var partition_num : Int = 2
+
+  /**
    * 数据 : _代表空值
+   * Spark RDD 不保存数据
    */
   var datas: List[Int] = _
 
   /**
-   * 处理逻辑
+   * 处理逻辑:Spark RDD 计算逻辑不可改变
    * 声明logic的类型(函数)  var logic : (Int) => Int
    */
   //val logic = (num : Int) => { num * 2 }
   //var logic : (Int) => Int = {_ * 2}
   //_代表空值
-  var logic: (Int) => Int = _
+  val logic: (Int) => Int = {_ * 100}
+
+  /**
+   * 生成分区
+   */
+  def splitPartition(): mutable.Queue[SparkMockTask] = {
+
+    // TODO: 判断条件
+    if (datas == null || logic == null || partition_num <= 0) {
+      null
+    }
+
+    // TODO: 生成subtask列表
+    var split_num = datas.length / partition_num
+    var partition_list: mutable.Queue[SparkMockTask] = new mutable.Queue[SparkMockTask]()
+
+    var data_start = 0
+    var data_end = 0
+    for (num <- 0 to partition_num - 1) {
+      // TODO: 模拟分区
+      data_start = data_start + (split_num * num)
+      if(data_start < 0){
+        data_start = 0
+      }
+      data_end = split_num * (num + 1) - 1
+      if (data_end + split_num > datas.length - 1){
+        data_end = datas.length - 1
+      }
+
+      var data_len : Int = data_end - data_start + 1
+      var task_datas: Array[Int] = new Array[Int](data_len)
+      var arr_index : Int = 0
+      for(index <- data_start to data_end){
+        task_datas(arr_index) = datas(index)
+        arr_index = arr_index + 1
+      }
+
+      // TODO:生成Executor
+      var task = new SparkMockTask
+      task.logic = logic
+      task.datas = task_datas.toList
+      partition_list += task
+    }
+
+    partition_list
+  }
 }
